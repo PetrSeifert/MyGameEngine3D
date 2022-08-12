dd3c0ccc254842161d94f8cccc8d0b3a740f0696
Petr Seifert <petr.seifert02@gmail.com>
2022-08-12 09:48:14 +0200

    update

---
+++ apps/sorting_algorithms/app.cpp
@@ -0,0 +1,171 @@
+#include <sorting_algorithms/app.hpp>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+struct GlobalUbo{
+    glm::mat4 projection{1.f};
+    glm::mat4 view{1.f};
+    glm::vec4 ambientLightColor{1.f, 1.f, 1.f, .02f};
+    glm::vec3 lightPosition{-5.f, -6.f, 0.f};
+    alignas(16) glm::vec4 lightColor{1.f};
+};
+
+MyModel::Builder boardSprite = {{
+    {{-1, -1, 0.f}, {0.01f, .01f, 0.01f}},
+    {{1, -1, 0.f}, {0.01f, .01f, 0.01f}},
+    {{1, 1, 0.f}, {0.01f, .01f, 0.01f}},
+    {{-1, 1, 0.f}, {0.01f, .01f, 0.01f}}},
+    {{0}, {2}, {3}, {0}, {1}, {2}}
+};
+
+MyModel::Builder greenSprite = {{
+    {{-1, -1, 0.f}, {0.0f, 1.0f, 0.0f}},
+    {{1, -1, 0.f}, {0.0f, 1.0f, 0.0f}},
+    {{1, 1, 0.f}, {0.0f, 1.0f, 0.0f}},
+    {{-1, 1, 0.f}, {0.0f, 1.0f, 0.0f}}},
+    {{0}, {2}, {3}, {0}, {1}, {2}}
+};
+
+MyModel::Builder redSprite = {{
+    {{-1, -1, 0.f}, {1.0f, 0.0f, 0.0f}},
+    {{1, -1, 0.f}, {1.0f, 0.0f, 0.0f}},
+    {{1, 1, 0.f}, {1.0f, 0.0f, 0.0f}},
+    {{-1, 1, 0.f}, {1.0f, 0.0f, 0.0f}}},
+    {{0}, {2}, {3}, {0}, {1}, {2}}
+};
+
+App::App(){
+    globalPool = MyDescriptorPool::Builder(myDevice)
+        .setMaxSets(MySwapChain::MAX_FRAMES_IN_FLIGHT)
+        .addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, MySwapChain::MAX_FRAMES_IN_FLIGHT)
+        .build();
+
+    uboBuffers = std::vector<std::unique_ptr<MyBuffer>>(MySwapChain::MAX_FRAMES_IN_FLIGHT);
+    for (int i = 0; i < uboBuffers.size(); i++){
+        uboBuffers[i] = std::make_unique<MyBuffer>(
+            myDevice,
+            sizeof(GlobalUbo),
+            1,
+            VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
+        );
+        uboBuffers[i]->map();
+    }
+
+    globalDescriptorSets = std::vector<VkDescriptorSet>(MySwapChain::MAX_FRAMES_IN_FLIGHT);
+    for (int i = 0; i < globalDescriptorSets.size(); i++){
+        auto bufferInfo = uboBuffers[i]->descriptorInfo();
+        MyDescriptorWriter(*globalSetLayout, *globalPool)
+            .writeBuffer(0, &bufferInfo)
+            .build(globalDescriptorSets[i]);
+    }
+    
+    setup();
+    currentTime = std::chrono::high_resolution_clock::now();
+}
+
+App::~App(){}
+
+void App::setup(){ 
+    loadGameObjects();
+}
+
+void App::run() {
+    while (!myWindow.shouldClose()){
+        glfwPollEvents();
+        cycles++;
+        auto newTime = std::chrono::high_resolution_clock::now();
+        float frameTime = std::chrono::duration<float, std::chrono::seconds::period>(newTime - currentTime).count();
+        currentTime = newTime;
+        sumFrameTime += frameTime;
+        totalTime += frameTime;
+
+        if (!insertionSort.sorted)
+            insertionSort.sortStep();
+
+        for (int i = 0; i < insertionSort.listSize; i++){
+            int id = i + 1;
+            // auto it = std::find(insertionSort.v.begin(),insertionSort.v.end(), gameObjects.at(id).transform.scale.y);
+
+            // if (std::distance(insertionSort.v.begin(), it) == insertionSort.currentIndex || std::distance(insertionSort.v.begin(), it) == insertionSort.minValueIndex){
+            //     std::shared_ptr<MyModel> myModel = std::make_shared<MyModel>(myDevice, redSprite);
+            //     gameObjects.at(id).model = myModel;
+            // }
+            // if (std::distance(insertionSort.v.begin(), it) == insertionSort.currentIndex - 1){
+            //     std::shared_ptr<MyModel> myModel = std::make_shared<MyModel>(myDevice, greenSprite);
+            //     gameObjects.at(id).model = myModel;
+            // }
+            // if (insertionSort.v.at(insertionSort.listSize - 1) == gameObjects.at(id).transform.scale.y){
+            //     std::shared_ptr<MyModel> myModel = std::make_shared<MyModel>(myDevice, greenSprite);
+            //     gameObjects.at(id).model = myModel;
+            // }
+
+            auto it = std::find(insertionSort.v.begin(),insertionSort.v.end(), gameObjects.at(id).transform.scale.y);
+            gameObjects.at(id).transform.translation = 
+            {-cameraSize + 0.5f + std::distance(insertionSort.v.begin(), it), 
+             gameObjects.at(id).transform.translation.y, gameObjects.at(id).transform.translation.z};
+        }
+
+
+        //set max frameTime for big lags
+
+        // cameraController.moveInPlaneXZ(myWindow.getGLFWWindow(), frameTime, viewerObject);
+
+        camera.setViewYXZ(viewerObject.transform.translation, viewerObject.transform.rotation);
+
+        float aspect = myRenderer.getAspectRatio();
+        camera.setOrthographicProjection(-aspect * cameraSize, aspect * cameraSize, -cameraSize, cameraSize, -1, 1);
+        //camera.setPerspectiveProjection(glm::radians(60.f), aspect, .1f, 100.f);
+
+        if (auto commandBuffer = myRenderer.beginFrame()){
+            int frameIndex = myRenderer.getFrameIndex();
+            FrameInfo frameInfo{
+                frameIndex,
+                frameTime,
+                commandBuffer,
+                camera,
+                globalDescriptorSets[frameIndex],
+                gameObjects
+            };
+
+            //update
+            GlobalUbo ubo{};
+            ubo.projection = camera.getProjection();
+            ubo.view = camera.getView();
+            uboBuffers[frameIndex]->writeToBuffer(&ubo);
+            uboBuffers[frameIndex]->flush();
+
+            //render
+            myRenderer.beginSwapChainRendererPass(commandBuffer);
+            simpleRendererSystem.renderGameObjects(frameInfo);
+            pointLightSystem.render(frameInfo);
+            myRenderer.endSwapChainRendererPass(commandBuffer);
+            myRenderer.endFrame();
+        }
+    }
+    vkDeviceWaitIdle(myDevice.device());
+}
+
+void App::loadGameObjects(){
+    std::shared_ptr<MyModel> myModel = std::make_shared<MyModel>(myDevice, greenSprite);
+    std::cout << insertionSort.listSize <<std::endl;
+    for (int i = 0; i < insertionSort.listSize; i++){
+        std::cout << insertionSort.v.at(i) << std::endl;
+        auto gameObj = MyGameObject::createGameObject();
+        gameObj.model = myModel;
+        gameObj.transform.scale = {1, insertionSort.v.at(i), 1};
+        gameObj.transform.translation = {-cameraSize + 0.5f + i, cameraSize - gameObj.transform.scale.y / 2, 0};
+        gameObj.color = {1, 0, 0};
+        gameObjects.emplace(gameObj.getId(), std::move(gameObj));
+    }
+
+    myModel = std::make_shared<MyModel>(myDevice, boardSprite);
+    auto gameObj = MyGameObject::createGameObject();
+    gameObj.model = myModel;
+    gameObj.transform.translation = {0, 0, .2f};
+    gameObj.transform.scale = {cameraSize*2, cameraSize*2, cameraSize*2};
+    gameObjects.emplace(gameObj.getId(), std::move(gameObj));
+}
---
+++ apps/sorting_algorithms/app.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <my_window.hpp>
+#include <my_device.hpp>
+#include <my_game_object.hpp>
+#include <my_renderer.hpp>
+#include <my_descriptors.hpp>
+#include <keyboard_movement_controller.hpp>
+#include <my_camera.hpp>
+#include <my_buffer.hpp>
+
+#include <systems/simple_renderer_system.hpp>
+#include <systems/point_light_system.hpp>
+
+//std
+#include <memory>
+#include <stdexcept>
+#include <array>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+
+#include <sorting_algorithms/sorting_algorithms.hpp>
+
+class App{
+    public:
+        static constexpr int WIDTH = 1280;
+        static constexpr int HEIGHT = 720;
+
+        App();
+        ~App();
+
+        //App(const App &) = delete;
+        //App& operator=(const App &) = delete;
+
+        void setup();
+        void run();
+    
+    private:
+        void loadGameObjects();
+        
+        MyWindow myWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+        MyDevice myDevice{myWindow};
+        MyRenderer myRenderer{myWindow, myDevice};
+
+        int cameraSize = 50;
+        int fpsLock = 5;
+        float minFrameTime = 0.f / fpsLock;
+        float sumFrameTime = 0;
+        float totalTime = 0.f;
+        int cycles = 0;
+
+        std::vector<std::unique_ptr<MyBuffer>> uboBuffers;
+        std::vector<VkDescriptorSet> globalDescriptorSets;
+        MyGameObject viewerObject = MyGameObject::createGameObject();
+        std::chrono::steady_clock::time_point currentTime;
+        KeyboardMovementController cameraController;
+        MyCamera camera{};
+        InsertionSort insertionSort{};
+
+
+        std::unique_ptr<MyDescriptorSetLayout> globalSetLayout = MyDescriptorSetLayout::Builder(myDevice)
+        .addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS)
+        .build();
+
+        SimpleRendererSystem simpleRendererSystem{
+            myDevice, 
+            myRenderer.getSwapChainRenderPass(), 
+            globalSetLayout->getDescriptorSetLayout()
+        };
+
+        PointLightSystem pointLightSystem{
+            myDevice, 
+            myRenderer.getSwapChainRenderPass(), 
+            globalSetLayout->getDescriptorSetLayout()
+        };
+
+        std::unique_ptr<MyDescriptorPool> globalPool{};
+        MyGameObject::Map gameObjects;  
+};
